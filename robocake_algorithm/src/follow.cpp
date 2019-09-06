19d876e22d2ee64a5ec46bf04de890d6e866a678
olegdiominov <4189469-olegdiominov@users.noreply.gitlab.com>
2019-09-06 15:50:12 +0300

    Implement following algorithm

--- robocake_algorithm/src/follow.cpp
+++ robocake_algorithm/src/follow.cpp
@@ -1,10 +1,30 @@
 #include "algorithm.h"
 
+int search_direction{1};
+
 geometry_msgs::Twist control(
                      std::unordered_map<std::string, double> proximity,
                      std::unordered_map<std::string, double> reflectance,
                      double threshold) {
   geometry_msgs::Twist msg;
-  msg.angular.z = 1;
+  double distance = -1;
+  if (proximity["front"] < 0.8) {
+    distance = proximity["front"];
+    msg.angular.z = 0;
+  } else if (proximity["left"] < 0.8) {
+    distance = proximity["left"];
+    search_direction = 1;
+    msg.angular.z = 1;
+  } else if (proximity["right"] < 0.8) {
+    distance = proximity["right"];
+    search_direction = -1;
+    msg.angular.z = -1;
+  }
+  if (distance > 0) {
+    auto v = std::min(std::abs(distance - threshold) / 0.05, 1.0) * 0.2;
+    msg.linear.x = distance < threshold ? -v : v;
+  } else {
+    msg.angular.z = search_direction * 0.5;
+  }
   return msg;
 }
